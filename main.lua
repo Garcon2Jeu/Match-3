e8b643d91546ab657c1a62c38ecd9204a2f9c167
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-11-20 14:56:06 +0100

    Implemented Key input

--- main.lua
+++ main.lua
@@ -5,10 +5,14 @@ function love.load()
 end
 
 function love.update(dt)
-
+    App:update()
 end
 
 function love.draw()
     Push:start()
     Push:finish()
+end
+
+function love.keypressed(key)
+    App.keysPressed[key] = true
 end
--- src/App.lua
+++ src/App.lua
@@ -3,6 +3,13 @@ App = Class {}
 function App:init()
     math.randomseed(os.time())
     self.setupWindow()
+
+    self.keysPressed = {}
+end
+
+function App:update()
+    self:quit()
+    self:flushKeys()
 end
 
 function App.setupWindow()
@@ -13,6 +20,20 @@ function App.setupWindow()
         resize = false,
         vsync = true
     })
+end
+
+function App:wasKeyPressed(key)
+    return self.keysPressed[key]
+end
+
+function App:quit()
+    if self:wasKeyPressed("escape") then
+        love.event.quit()
+    end
+end
+
+function App:flushKeys()
+    self.keysPressed = {}
 end
 
 return App()
