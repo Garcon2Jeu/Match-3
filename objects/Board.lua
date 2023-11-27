33116a4800f6a738b8d7791b9313c59a483c7e45
Garcon2Jeu <104232891+Garcon2Jeu@users.noreply.github.com>
2023-11-27 12:44:40 +0100

    Added RemoveDropReplace

--- objects/Board.lua
+++ objects/Board.lua
@@ -1,9 +1,11 @@
 Board = Class {}
 
 local boardSize = 8
+local xOffset = CENTER_WIDTH - 16
+local yOffset = 16
 
 function Board:init()
-    self.grid = self.factory(CENTER_WIDTH - 16, 16)
+    self.grid = self.factory(xOffset, yOffset)
 end
 
 function Board:draw(board)
@@ -44,22 +46,7 @@ function Board:areTilesAdjacent(tile1, tile2)
     return math.abs(tile1.row - tile2.row) + math.abs(tile1.column - tile2.column) == 1
 end
 
-function Board:swapTiles(tile1, tile2)
-    -- Swap XY
-    local tempX, tempY =
-        self.grid[tile1.row][tile1.column].x,
-        self.grid[tile1.row][tile1.column].y
-    Timer.tween(.25, {
-        [self.grid[tile1.row][tile1.column]] = {
-            x = self.grid[tile2.row][tile2.column].x,
-            y = self.grid[tile2.row][tile2.column].y,
-        },
-        [self.grid[tile2.row][tile2.column]] = {
-            x = tempX,
-            y = tempY,
-        },
-    })
-
+function Board:swapTiles(tile1, tile2, next)
     -- Swap Row/Column
     local tempRow, tempColumn =
         self.grid[tile1.row][tile1.column].row,
@@ -72,10 +59,41 @@ function Board:swapTiles(tile1, tile2)
     self.grid[tile2.row][tile2.column].column =
         tempRow, tempColumn
 
+
     -- Swap Grid Position
     local tempTile = self.grid[tile1.row][tile1.column]
     self.grid[tile1.row][tile1.column] = self.grid[tile2.row][tile2.column]
     self.grid[tile2.row][tile2.column] = tempTile
+
+
+    -- Swap XY
+    local tempX, tempY =
+        self.grid[tile1.row][tile1.column].x,
+        self.grid[tile1.row][tile1.column].y
+
+    local tweeningData = {
+        [self.grid[tile1.row][tile1.column]] = {
+            x = self.grid[tile2.row][tile2.column].x,
+            y = self.grid[tile2.row][tile2.column].y,
+        },
+        [self.grid[tile2.row][tile2.column]] = {
+            x = tempX,
+            y = tempY,
+        },
+    }
+
+    Timer.tween(.25, tweeningData):finish(next)
+end
+
+function Board:RemoveDropReplace()
+    return function()
+        local matches = self:getAllMatches()
+
+        if #matches > 0 then
+            self:removeMatches(matches)
+            self:dropTiles(matches)
+        end
+    end
 end
 
 function Board:getAllMatches(grid)
@@ -122,13 +140,58 @@ function Board:getMatchesBy(direction, grid)
     return totalMatches
 end
 
--- KEEP UP THE GOOD JOB BITCH!!!!!!!--------------------------
 function Board:removeMatches(matches)
     for key, match in pairs(matches) do
         for key, tile in pairs(match) do
             self.grid[tile.row][tile.column] = nil
         end
     end
+end
+
+function Board:dropTiles(matches)
+    local tweeningData = {}
+
+    for key, match in pairs(matches) do
+        for key, emptyTile in pairs(match) do
+            local tilesToDrop = {}
+            local row = emptyTile.row - 1
+
+            while row >= 1 do
+                if self.grid[row][emptyTile.column] then
+                    table.insert(tilesToDrop, self.grid[row][emptyTile.column])
+                end
+
+                row = row - 1
+            end
+
+            row = emptyTile.row
+
+            while row >= 1 do
+                if #tilesToDrop > 0 then
+                    self.grid[row][emptyTile.column] = tilesToDrop[1]
+                    self.grid[row][emptyTile.column].row = row
+                    table.remove(tilesToDrop, 1)
+                else
+                    self.grid[row][emptyTile.column] = emptyTile
+                    self.grid[row][emptyTile.column].y = -AtlasManager.getTileSize()
+                    self.grid[row][emptyTile.column].row = row
+                    self.grid[row][emptyTile.column].color = math.random(Atlas.getTotalColors())
+                end
+
+                tweeningData[self.grid[row][emptyTile.column]] = {
+                    y = AtlasManager.getTileSize() * (row - 1) + Board.getYoffset(),
+                }
+
+                row = row - 1
+            end
+        end
+    end
+
+    Timer.tween(.25, tweeningData):finish(self:RemoveDropReplace())
+end
+
+function Board.getYoffset()
+    return yOffset
 end
 
 -- function PlayState:getRowMatches()
@@ -182,3 +245,12 @@ end
 
 --     return totalMatches
 -- end
+
+--INSTANTLY SWAP XY
+-- self.grid[tile1.row][tile1.column].x,
+-- self.grid[tile1.row][tile1.column].y =
+--     self.grid[tile2.row][tile2.column].x,
+--     self.grid[tile2.row][tile2.column].y
+-- self.grid[tile2.row][tile2.column].x,
+-- self.grid[tile2.row][tile2.column].y =
+--     tempX, tempY
--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -17,34 +17,23 @@ function PlayState:update(dt)
     self.player:update(dt)
 
     if App:wasKeyPressed("return") and self:isSwapPossible() then
-        self.board:swapTiles(
-            self.player.selected,
-            self.player.cursor
-        )
-        self.player:unselect()
-        local matches = self.board:getAllMatches()
-        self.board:removeMatches(matches)
+        Chain(
+            function(next)
+                self.board:swapTiles(
+                    self.player.selected,
+                    self.player.cursor,
+                    next
+                )
+                self.player:unselect()
+            end,
+            self.board:RemoveDropReplace()
+        )()
     end
 end
 
 function PlayState:draw()
     self.board:draw()
     self.player:draw(self.board)
-
-    ------------------------------------------------------DEBUG-------------------------------------------------------------------
-    -- love.graphics.print(tostring(#self.board:getMatchesBy("row")), 50, 50)
-    -- love.graphics.print(tostring(#self.board:getMatchesBy("column")), 50, 70)
-    -- love.graphics.print(tostring(#self.board:getAllMatches()), 50, 90)
-
-    -- if #self.board:getAllMatches() > 0 then
-    --     love.graphics.print(tostring(self.board:getAllMatches()[1][1].row), 50, 50)
-    --     love.graphics.print(tostring(self.board:getAllMatches()[1][1].column), 50, 70)
-    --     love.graphics.print(tostring(self.board:getAllMatches()[1][2].row), 50, 90)
-    --     love.graphics.print(tostring(self.board:getAllMatches()[1][2].column), 50, 110)
-    --     love.graphics.print(tostring(self.board:getAllMatches()[1][3].row), 50, 130)
-    --     love.graphics.print(tostring(self.board:getAllMatches()[1][3].column), 50, 150)
-    -- end
-    ------------------------------------------------------DEBUG-------------------------------------------------------------------
 end
 
 function PlayState:exit()
